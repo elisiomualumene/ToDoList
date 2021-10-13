20f24ccc6839893c338f267b4db86510afbe0ef3
Elísio Mualumene <elisiogabrielmualumene@gmail.com>
2021-10-13 20:12:19 +0100

    working with validations

--- functionality/functionality.js
+++ functionality/functionality.js
@@ -1,4 +1,4 @@
-let inputfield = document.getElementById('input')
+let inputfield = document.getElementById('input2')
 // Here Get the INPUT
 let addtodo = document.getElementById('addtodo')
 // Here Get the Button
@@ -7,11 +7,16 @@ let here_ = document.getElementById('nothing')
 
 // Here a declare a function, with a addEventListener
 addtodo.addEventListener('click', function(){
-        // Here a creat a Element Paragraph, and a declare paragraph too
-    var paragraph = document.createElement('p')
+    if(inputfield == ""){
+        document.alert('Please Enter a Value')
+        return false;
+    }else{    
+    // Here a creat a Element Paragraph, and a declare paragraph too
+    var paragraph = document.createElement('li')
         //Here I put the value of input in a variable 'Paragraph with InnerText'
     paragraph.innerText = inputfield.value
         //And Here I add Paragraph to Container 'here_'
     here_.appendChild(paragraph)
         //Finish Here
+    }
 })
