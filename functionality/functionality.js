4b7e2d498c91609dfa9475e11d217477bfd1a16f
Elísio Mualumene <elisiogabrielmualumene@gmail.com>
2021-10-13 17:29:44 +0100

    JavaScript Almost Finish
    Finish the Brain of the TO DO LIST

--- functionality/functionality.js
+++ functionality/functionality.js
@@ -1,3 +1,17 @@
-let inputfield = document.querySelector('input#input')
+let inputfield = document.getElementById('input')
+// Here Get the INPUT
+let addtodo = document.getElementById('addtodo')
+// Here Get the Button
+let here_ = document.getElementById('nothing')
+// And Here Get the Container 
 
-alert('ok')
+// Here a declare a function, with a addEventListener
+addtodo.addEventListener('click', function(){
+        // Here a creat a Element Paragraph, and a declare paragraph too
+    var paragraph = document.createElement('p')
+        //Here I put the value of input in a variable 'Paragraph with InnerText'
+    paragraph.innerText = inputfield.value
+        //And Here I add Paragraph to Container 'here_'
+    here_.appendChild(paragraph)
+        //Finish Here
+})
